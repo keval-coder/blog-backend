cb7c77ec0535e3c614f6af345966c21b1364c021
MK <kevalmendapara1997@gmail.com>
2023-10-05 23:56:59 +0530

    cors fixed 2

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://blog-frontend-wheat-theta.vercel.app/',
+    credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
 
